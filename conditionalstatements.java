8aa29addb76fd5faec791f2f65980cc6e69473be
mamathaboga <mamathaboga328@gmail.com>
2026-07-26 21:14:20 +0530

    added conditional statement programs

---
+++ conditionalstatements.java
@@ -0,0 +1,28 @@
+public class conditionalstatements {
+    public static void main(String [] args){
+      /*   int x=8;
+        int y=17;
+        int z=9;
+        if(x>y&&x>z)
+            System.out.println(x);
+        else if(y>x&&y>z)
+            System.out.println(y);
+        else
+            System.out.println(z);*/
+        //switch statements
+       /*  String day="saturday";
+        switch(day){
+            case "saturday","sunday":
+                System.out.println("6am");
+                break;
+                case "monday":
+                    System.out.println("8am");
+                    break;
+                default:
+                    System.out.println("7am");*/
+
+        }
+
+    }
+    
+
