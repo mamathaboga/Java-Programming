9e1140dde507641e8eb0060356532bdbde05ebc0
mamathaboga <mamathaboga328@gmail.com>
2026-07-26 19:08:57 +0530

    Initial Java programs

---
+++ Variables.java
@@ -0,0 +1,9 @@
+public class Variables{
+    public static void main(String [] args){
+        int num=3;
+        int num1=4;
+        int num2=5;
+        System.out.println(num);
+        System.out.println(num1+num2);
+    }
+}
