4740b0d52a1869c72f2df1a85115c63e3fb0333e
mamathaboga <mamathaboga328@gmail.com>
2026-07-26 21:00:19 +0530

    added operators programs

---
+++ operators.java
@@ -0,0 +1,32 @@
+public class operators {
+    public static void main(String [] args){
+       /* //Arithmetic operators(+,-,*,/,%)
+        int a=10;
+        int b=20;
+        System.out.println("Addition="+(a+b));
+        System.out.println("subtraction="+(a-b));
+        //unary operators
+        a++;
+        b--;
+        System.out.println(a);
+        System.out.println(b);*/
+    //relational operators
+   /*  int x=6;
+    int y=5;
+    boolean result=x<y;
+    System.out.println(result);*/
+    //logical operators
+    /*int x=7;
+    int y=5;
+    int a=5;
+    int b=9;
+    boolean result=x>y && a>b;
+    System.out.println(result);*/
+    //ternery operator
+    /*int age=20;
+    String result=(age>=18)?"Eligible to vote":"Not Eligible";
+    System.out.println(result);*/
+
+    }
+    
+}
