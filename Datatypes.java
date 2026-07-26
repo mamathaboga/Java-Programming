145bcc76e48257a169b975bb3f9d581ea5abbefa
mamathaboga <mamathaboga328@gmail.com>
2026-07-26 20:33:05 +0530

    added datatypes program

---
+++ Datatypes.java
@@ -0,0 +1,13 @@
+public class Datatypes {
+    public static void main(String[] args){
+        //type casting and type conversion
+        byte b=127;
+        int a=12;
+        b=(byte)a;//type casting(explicit)
+        a=b;//type conversion (implicit)
+        System.out.println("a= "+a);
+        System.out.println("b= "+b);
+
+    }
+    
+}
