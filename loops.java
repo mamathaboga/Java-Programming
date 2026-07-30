dae8417f49ef658f11fd9336abf60928e842a914
mamathaboga <mamathaboga328@gmail.com>
2026-07-30 21:55:18 +0530

    added loops programs

---
+++ loops.java
@@ -0,0 +1,45 @@
+public class loops {
+    public static void main(String [] args){
+        //while loop
+        /*int i=1;
+        while(i<=4){
+            System.out.println("Hi"+i);
+            i++;
+        }
+        System.out.println("Bye"+i);*/
+        /*int i=1;
+        while(i<=4){
+            System.out.println("Hi"+i);
+            int j=1;
+            while(j<=3){
+                System.out.println("Hello"+j);
+                j++;
+            }
+            i++;
+        }
+        System.out.println("Bye"+i);*/
+        //do while loop
+        /*int i=5;
+        do{
+            System.out.println("Hi"+" " +i);
+            i++;
+        }
+        while(i<=4);
+        System.out.println(i);
+    }   
+}*/
+//for loop
+/*for(int i=1;i<=4;i++)
+    System.out.println("Hi"+" "+i);*/
+/*for(int i=4;i>=1;i--)
+    System.out.println("Hi"+" "+i);*/
+for(int i=1;i<=5;i++){
+    System.out.println("DAY"+i);
+for(int j=1;j<=9;j++){
+    System.out.println(" "+(j+8)+"-"+(j+9));
+}
+}
+}
+}
+
+
