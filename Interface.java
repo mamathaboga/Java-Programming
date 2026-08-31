5ef2ae2bdaca892222b453ea18d1ebf3ca212b1c
mamathaboga <mamathaboga328@gmail.com>
2026-08-31 22:14:07 +0530

    Added programs related to interface in java

--- Interface.java
+++ Interface.java
@@ -1,4 +1,4 @@
-interface A{
+/*interface A{
     int age=44;//by default these variables are final and static
     String name="Mumbai";//that means fixed
     void show();
@@ -20,4 +20,37 @@ public class Interface{
         obj.config();
         
     }
+}*/
+//functional interface
+// functional interface consists of only single abstract method
+/*interface A{
+    void show();
 }
+class B implements A{
+    public void show(){
+        System.out.println("in show");
+    }  
+}
+public class Interface{
+    public static void main(){
+        A obj=new B();
+        obj.show();
+    }
+}*/
+//also we can create obj of A by creating its own implementation(abstract and anonymous inner class)
+interface A{
+    void show();
+}
+public class Interface{
+    public static void main(String[] args){
+A obj=new A(){
+        public void show(){
+            System.out.println("in show");
+        }
+    };
+    obj.show();
+
+    }
+}
+
+
